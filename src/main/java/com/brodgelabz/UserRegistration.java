2a84ff6d039737df5181ba60f053c2bcc2543025
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-22 07:53:54 +0530

    added email validation

--- src/main/java/com/brodgelabz/UserRegistration.java
+++ src/main/java/com/brodgelabz/UserRegistration.java
@@ -20,6 +20,13 @@ interface Validation {
             System.out.println(isValidLastName.validate("^[A-Z]{1}[a-z]{2,}$", "Nangare"));
 
 
+            Validation isValidEmail = (pattern, email) -> {
+                return "Email is " + Pattern.compile(pattern).matcher(email).matches();
+            };
+            System.out.println(isValidEmail.validate("^[a-z]+[+-_.]*[a-z]*[@][a-z]+[.][a-z]{2,4}[.]*([a-z]{2})*$",
+                    "sagarnangare@gmail.com"));
+
+
         }
 
     }
