94b018a79e5574755ab2ae819e5962d73a9fc077
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-22 07:59:37 +0530

    password validation

--- src/main/java/com/brodgelabz/UserRegistration.java
+++ src/main/java/com/brodgelabz/UserRegistration.java
@@ -31,6 +31,10 @@ interface Validation {
             };
             System.out.println(isValidMobileNumber.validate("^[91]+[ ]?[6-9]{1}[0-9]{9}$", "91 9619819812"));
 
+            Validation isValidPassword1 = (pattern, password) -> {
+                return "Password rule is " + Pattern.compile(pattern).matcher(password).matches();
+            };
+            System.out.println(isValidPassword1.validate("^[0-9a-zA-Z!,@#$&*().]{8,}$", "Sagar7@"));
 
         }
 
